c6d5734cfcc5a6c802ee170de0b7c802c819f937
Crystel <crysvane05@estudiantec.cr>
2019-10-13 11:42:22 -0600

    Clase principal

---
+++ Principal.h
@@ -0,0 +1,15 @@
+#ifndef PRINCIPAL_H
+#define PRINCIPAL_H
+
+
+class Principal
+{
+    public:
+        Principal(){
+        }
+        bool validarCliente(Pagina *r, string ced);
+
+    private:
+};
+
+#endif // PRINCIPAL_H
